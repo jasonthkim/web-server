79fbcb38547fc967d61337976d930f8d8b5d2bb8
Jason Kim <jasonthkim@gmail.com>
2015-10-26 23:57:35 -0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		// new Date().toString()
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+//app.use(middleware.requireAuthentication);
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('<h1>About Us</h1>');
+});
+
+// This command below is how to find full directory 
+//console.log(__dirname);
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(PORT, function () {
+	console.log('Express server started on port #: ' + PORT + ' !!!');
+});
